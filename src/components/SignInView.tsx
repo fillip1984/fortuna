3f6d3e22d62dc25c6198dfe6fec11241a44d8eef
fillip1984 <fillip1984@gmail.com>
2025-10-14 13:17:14 -0400

    about to switch to better auth

--- src/components/SignInView.tsx
+++ src/components/SignInView.tsx
@@ -2,8 +2,14 @@
 
 import { signIn } from "next-auth/react";
 import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { useState } from "react";
 
 export default function SignInView() {
+  const [email, setEmail] = useState("fillip1984@gmail.com");
+  const [password, setPassword] = useState("Fortis et liber!1");
+
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <h1 className="text-3xl font-bold">Welcome to Fortuna</h1>
@@ -12,8 +18,33 @@ export default function SignInView() {
         onClick={() => signIn("google")}
         className="text-muted-foreground cursor-pointer"
       >
-        Please sign in to continue
+        Sign in with Google
       </Button>
+      <hr />
+      or
+      <hr />
+      <div className="my-4 flex w-1/3 flex-col gap-2">
+        <h3>Sign in with email/password</h3>
+        <Label className="text-muted-foreground">Email</Label>
+        <Input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Label className="text-muted-foreground">Password</Label>
+        <Input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          onClick={() => signIn("credentials", { email, password })}
+          disabled={!email || !password}
+          className="mt-4"
+        >
+          Sign In
+        </Button>
+      </div>
     </div>
   );
 }
--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,8 +1,9 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-
 import { db } from "~/server/db";
+import bcrypt from "bcryptjs";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -32,6 +33,54 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        try {
+          console.log("Credentials:::::::::::::::::::::::::::", credentials);
+          let user = null;
+          const email: string = (credentials?.email as string) ?? "";
+          const password: string = (credentials?.password as string) ?? "";
+
+          if (!email || !password) {
+            throw new Error("Email and password are required");
+          }
+          // const x = bcrypt.hashSync("Fortis et liber!1", 10);
+          // const y = bcrypt.hashSync("Fortis et liber!1", 10);
+          // const pwHash = await bcrypt.hash(password, 10);
+
+          // console.log({ x, y, pwHash });
+          // console.log({ msg: "Testing", pwHash });
+          // console.log({ email, password });
+          user = await db.user.findFirst({
+            where: {
+              email: email,
+            },
+          });
+
+          if (!user?.password) {
+            console.log("::::::::::::::PWHASH NOT FOUND");
+            throw new Error("Invalid email or password");
+          }
+
+          if (bcrypt.compareSync(password, user.password)) {
+            console.log("::::::::::::::User authenticated successfully");
+            return user;
+          }
+
+          console.log("::::::::::::::Conditions not met");
+
+          // default to throwing an error
+          throw new Error("Invalid email or password");
+        } catch (error) {
+          console.error("Error in authorize:", error);
+          return null;
+        }
+      },
+    }),
     GoogleProvider,
     /**
      * ...add more providers here.
@@ -48,7 +97,8 @@ export const authConfig = {
     signIn: async (params) => {
       const { account, user } = params;
 
-      // return true; // TEMPORARY: allow all sign-ins
+      console.log({ msg: ":::::::", account, user });
+      return true; // TEMPORARY: allow all sign-ins
 
       if (account?.provider === "email") {
         const existingUser = await db.user.findFirst({
