e5b37b6105cc2027fce0c5d6b68ecee6f8add4ab
Phillip Williams <fillip1984@gmail.com>
2025-12-04 19:57:33 -0500

    updated to latest packages

--- src/components/nav/SideNav.tsx
+++ src/components/nav/SideNav.tsx
@@ -78,9 +78,13 @@ export default function SideNav() {
     setDraggabledCollections(collections ?? []);
   }, [collections, setDraggabledCollections]);
 
+  if (!session) {
+    return null;
+  }
+
   return (
     <>
-      <nav className="flex flex-col shrink-0 gap-2 overflow-hidden">
+      <nav className="flex shrink-0 flex-col gap-2 overflow-hidden">
         <div className="flex flex-1 flex-col gap-2 overflow-y-auto p-2 pb-12">
           <h4 className="mx-auto italic">fortuna</h4>
           <div className="grid w-[280px] grid-cols-1 gap-1 select-none md:grid-cols-2">
